beff3325964409b97b307e992b09971dbae3cd86
Bilgehan Zeki ÖZAYTAÇ <zekiozaytac@hotmail.com.tr>
2019-08-10 15:16:04 +0300

    Add project files.

---
+++ Harita/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+using Harita.Properties;
+
+namespace Harita
+{
+  public partial class Form1 : Form
+  {
+    private List<ImagePoint> landmarks;
+    private Bitmap markerImage;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+
+    private IDictionary<string, Image> ImageCache { get; set; }
+
+    private bool IsUpdatingMap { get; set; }
+
+    private int Layer { get; set; }
+
+    private List<MapLayerData> LayerData { get; set; }
+
+    private bool ResetZoomOnUpdate { get; set; }
+
+    private int VirtualZoom { get; set; }
+
+    private int FindNearestLayer(int zoom)
+    {
+      int result;
+
+      result = -1;
+
+      for (var i = 0; i < LayerData.Count; i++)
+      {
+        MapLayerData data;
+
+        data = LayerData[i];
+
+        if (zoom >= data.LowerZoom && zoom < data.UpperZoom)
+        {
+          result = i;
+          break;
+        }
+      }
+
+      return result;
+    }
+
+    private void UpdateMap()
+    {
+      if (!IsUpdatingMap)
+      {
+        int mapLayer;
+
+        IsUpdatingMap = true;
+
+        mapLayer = FindNearestLayer(VirtualZoom);
+
+        if (mapLayer != -1 && mapLayer != Layer)
+        {
+          MapLayerData data;
+          Image newImage;
+          RectangleF currentViewport;
+          RectangleF newViewport;
+          Size currentSize;
+          float vAspectRatio;
+          float hAspectRatio;
+
+          Layer = mapLayer;
+          data = LayerData[mapLayer];
+          //this.mapNameToolStripStatusLabel.Text = data.Name;
+
+          newImage = GetMapImage(data.Name);
+          currentViewport = imageBox.GetSourceImageRegion();
+          currentSize = imageBox.Image.Size;
+
+          hAspectRatio = newImage.Width / (float) currentSize.Width;
+          vAspectRatio = newImage.Height / (float) currentSize.Height;
+
+          imageBox.BeginUpdate();
+          imageBox.Image = newImage;
+          newViewport = new RectangleF(
+            currentViewport.X * hAspectRatio,
+            currentViewport.Y * vAspectRatio,
+            currentViewport.Width * hAspectRatio,
+            currentViewport.Height * vAspectRatio);
+          imageBox.ZoomToRegion(newViewport);
+
+          if (ResetZoomOnUpdate)
+          {
+            ResetZoomOnUpdate = false;
+            imageBox.Zoom = 100;
+            imageBox.CenterToImage();
+          }
+
+          imageBox.EndUpdate();
+        }
+
+        IsUpdatingMap = false;
+      }
+    }
+
+    private void AddLandmark(string name, Point point)
+    {
+      Debug.Print("Added landmark: {0}", point);
+
+      landmarks.Add(new ImagePoint(name, new PointF(point.X, point.Y)));
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+
+      markerImage = Resources.MapMarker;
+
+      landmarks = new List<ImagePoint>();
+      AddLandmark("Britain", new Point(182, 209));
+      AddLandmark("Trinsic", new Point(234, 342));
+      AddLandmark("Minoc", new Point(309, 62));
+
+
+      //this.imagePoints = new List<ImagePoint>();
+      //this.imagePoints.AddRange(
+      //  new[]
+      //    {
+      //      new ImagePoint { color = "Red", label = "Test", location = new PointF(2360 / 8, 2840 / 8) },
+      //      new ImagePoint { color = "Red", label = "Test", location = new PointF(400, 400) }
+      //    });
+      LayerData = new List<MapLayerData>();
+
+      // add some map layers for the different zoom levels
+      AddLayer("MAP25166902-8", int.MinValue, 2, 1);
+      AddLayer("MAP25166902-4", 2, 4, 2);
+      AddLayer("MAP25166902-2", 4, 8, 4);
+      AddLayer("MAP25166902-1", 8, int.MaxValue, 8);
+
+      // load the lowest detail map
+      imageBox.Image = GetMapImage("MAP25166902-8");
+
+      // now zoom in a bit
+      VirtualZoom = 1;
+      UpdateMap();
+      imageBox.Zoom = 100;
+      imageBox.CenterAt(234, 342);
+
+      // imageBox.ZoomToFit();
+    }
+
+    private void AddLayer(string name, int lowerZoom, int upperZoom, int zoomLevel)
+    {
+      // The larger map sizes (>map50) are 80MB, so I'm not including them in the GitHub repository.
+      // Therefore, just silently skip any missing maps without raising an error
+      if (File.Exists(GetMapFileName(name)))
+      {
+        MapLayerData data;
+
+        data = new MapLayerData();
+        data.Name = name;
+        data.UpperZoom = upperZoom;
+        data.LowerZoom = lowerZoom;
+        data.ZoomLevel = zoomLevel;
+
+        LayerData.Add(data);
+      }
+    }
+
+    private Image GetMapImage(string name)
+    {
+      Image result;
+
+      if (ImageCache == null) ImageCache = new Dictionary<string, Image>();
+
+      if (!ImageCache.TryGetValue(name, out result))
+      {
+        //this.SetStatus("Loading image...");
+
+        result = Image.FromFile(GetMapFileName(name));
+
+        ImageCache.Add(name, result);
+
+        //this.SetStatus(string.Empty);
+      }
+
+      //this.SetMessage(string.Format("Switching to image {0}.jpg", name));
+
+      return result;
+    }
+
+    private string GetMapFileName(string name)
+    {
+      return Path.GetFullPath(
+        Path.Combine(
+          Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\maps"),
+          Path.ChangeExtension(name, ".bmp")));
+    }
+
+    private struct MapLayerData
+    {
+      #region Public Properties
+
+      public int LowerZoom { get; set; }
+
+      public string Name { get; set; }
+
+      public int UpperZoom { get; set; }
+
+      public int ZoomLevel { get; set; }
+
+      #endregion
+    }
+
+    private void ImageBox_Paint(object sender, PaintEventArgs e)
+    {
+      Graphics g;
+      GraphicsState originalState;
+      Size scaledSize;
+      Size originalSize;
+      Size drawSize;
+      bool scaleAdornmentSize;
+
+      scaleAdornmentSize = false;
+
+      g = e.Graphics;
+
+      originalState = g.Save();
+
+      // Work out the size of the marker graphic according to the current zoom level
+      originalSize = this.markerImage.Size;
+      scaledSize = this.imageBox.GetScaledSize(originalSize);
+      drawSize = scaleAdornmentSize ? scaledSize : originalSize;
+
+
+      foreach (var landmark in this.landmarks)
+      {
+        PointF location;
+
+        var yeniLandmark = landmark;
+
+        var findNearestLayer = this.FindNearestLayer(this.VirtualZoom);
+        if (this.LayerData.Count <= findNearestLayer || findNearestLayer >= 0)
+        {
+
+
+          var div = this.LayerData[findNearestLayer].ZoomLevel;
+          var yeniLandmarkLocation = yeniLandmark.location;
+          yeniLandmarkLocation.X *= div;
+          yeniLandmarkLocation.Y *= div;
+
+          // Work out the location of the marker graphic according to the current zoom level and scroll offset
+          location = this.imageBox.GetOffsetPoint(yeniLandmarkLocation);
+
+          // adjust the location so that the image is displayed above the location and centered to it
+          location.Y -= drawSize.Height;
+          location.X -= drawSize.Width >> 1;
+
+          // Draw the marker
+          g.InterpolationMode = InterpolationMode.NearestNeighbor;
+          g.DrawImage(
+            this.markerImage,
+            new Rectangle((int)location.X, (int)location.Y, drawSize.Width, drawSize.Height),
+            new Rectangle(Point.Empty, originalSize),
+            GraphicsUnit.Pixel);
+        }
+      }
+
+      g.Restore(originalState);
+    }
+  }
+}
---
+++ Harita/ImagePoints.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Drawing;
+using Cyotek.Windows.Forms;
+
+namespace Harita
+{
+  public static class GlobalSettings
+  {
+    public static Pen crossPen = new Pen(Color.Red);
+    public static Pen profilPen = new Pen(Color.Yellow, 1.5f);
+    public static Pen calibratePen = new Pen(Color.Red, 2);
+    public static Pen selectedPen = new Pen(Color.Fuchsia, 2);
+    public static Pen linesPen = new Pen(Color.Green, 2);
+    public static Font fontLines = new Font("Thaoma", 11);
+    public static Pen indexBrush = new Pen(Color.Green, 2);
+    public static Font fontDescription = new Font("Thaoma", 11);
+    public static Pen descriptionBrush = new Pen(Color.Red, 2);
+    public static Font fontPoints = new Font("Thaoma", 11);
+    public static int lineLength = 20;
+
+    public static int roundTime = 4;
+    public static int roundPitchPlunge = 2;
+    public static int roundOthers = 2;
+
+    public static bool desc = true;
+    public static bool points = true;
+    public static bool pointsDesc = true;
+    public static bool lines = true;
+    public static bool linesDesc = true;
+    public static bool lineFringeNumber = true;
+    public static bool profil;
+
+    // ratio = distancebetweencalibrationpoints/realdistance [px/mm]
+    public static double ratio = double.NaN;
+
+    public static bool fringeLabels = false;
+    public static bool fringeLabelsPanel = false;
+    public static int fringeCircleSize = 3;
+    public static float fringeStep = 1;
+    public static Color fringeLabelsColor = Color.Green;
+    public static Font fringeLabelsFont = new Font("Thaoma", 11);
+    public static float fringeLabelsFrom = 0;
+    public static float fringeLabelsTo = 1;
+  }
+
+  [Serializable]
+  public class ImagePoint
+  {
+    public string color;
+    public bool exist = true;
+    public string label;
+    public PointF labelLocation;
+    public float labelOffsetX = 0;
+    public float labelOffsetY = -30;
+    public PointF location;
+
+    [NonSerialized] public Pen pen;
+
+    public float ratio = 10;
+    public SizeF sizeString;
+
+    public ImagePoint()
+    {
+    }
+
+    public ImagePoint(string name, PointF l)
+    {
+      location = l;
+      label = name;
+    }
+
+    public ImagePoint(Pen p, PointF l)
+    {
+      pen = p;
+      location = l;
+    }
+
+    public void DrawToGraphics(Graphics g, PointF center, bool isFringe = false, bool isSelected = false)
+    {
+      if (isFringe)
+      {
+        if (isSelected)
+          g.DrawEllipse(new Pen(GlobalSettings.selectedPen.Color, 1.5f), center.X - GlobalSettings.fringeCircleSize,
+            center.Y - GlobalSettings.fringeCircleSize, GlobalSettings.fringeCircleSize * 2,
+            GlobalSettings.fringeCircleSize * 2);
+        else
+          g.DrawEllipse(new Pen(GlobalSettings.fringeLabelsColor, 1.5f), center.X - GlobalSettings.fringeCircleSize,
+            center.Y - GlobalSettings.fringeCircleSize, GlobalSettings.fringeCircleSize * 2,
+            GlobalSettings.fringeCircleSize * 2);
+      }
+      else
+      {
+        g.DrawEllipse(pen, center.X - ratio, center.Y - ratio, ratio * 2, ratio * 2);
+      }
+    }
+
+    public void DrawToGraphics(Graphics g, PointF center, int n)
+    {
+      g.DrawEllipse(pen, center.X - ratio * n, center.Y - ratio * n, ratio * n * 2, ratio * n * 2);
+    }
+
+    public PointF GetLabelLocation(ImageBox i)
+    {
+      return new PointF(i.GetOffsetPoint(new PointF(location.X + labelOffsetX, location.Y + labelOffsetY)).X,
+        i.GetOffsetPoint(new PointF(location.X + labelOffsetX, location.Y + labelOffsetY)).Y +
+        (float) (8 * ((i.ZoomFactor - 0.7) / 0.3)));
+    }
+
+    public PointF GetLabelLocation()
+    {
+      return new PointF(location.X + labelOffsetX, location.Y + labelOffsetY);
+    }
+
+    public bool IsHitLabel(PointF p, ImageBox i, bool isFringeLabel = false)
+    {
+      PointF offsetPoint;
+      if (isFringeLabel)
+      {
+        var size = i.CreateGraphics().MeasureString(label, GlobalSettings.fringeLabelsFont);
+        var l = i.GetOffsetPoint(location);
+        //click = i.GetOffsetPoint(p);
+        offsetPoint = i.GetOffsetPoint(new PointF(location.X + labelOffsetX, location.Y + labelOffsetY));
+        if (p.X < offsetPoint.X + size.Width
+            && p.X > offsetPoint.X - 4
+            && p.Y < offsetPoint.Y + size.Height + (float) (8 * ((i.ZoomFactor - 0.7) / 0.3))
+            && p.Y > offsetPoint.Y + (float) (8 * ((i.ZoomFactor - 0.7) / 0.3)))
+          return true;
+        return false;
+      }
+
+      p = i.GetOffsetPoint(p);
+      offsetPoint = i.GetOffsetPoint(new PointF(location.X + labelOffsetX, location.Y + labelOffsetY));
+      if (p.X < offsetPoint.X + sizeString.Width
+          && p.X > offsetPoint.X
+          && p.Y < offsetPoint.Y + sizeString.Height + (float) (8 * ((i.ZoomFactor - 0.7) / 0.3))
+          && p.Y > offsetPoint.Y + (float) (8 * ((i.ZoomFactor - 0.7) / 0.3)))
+        return true;
+      return false;
+    }
+
+    public bool IsHit(Point p, ImageBox i, int div, bool isFringe = false)
+    {
+      if (isFringe)
+      {
+        p = i.PointToImage(p);
+        p.X /= div;
+        p.Y /= div;
+        if ((p.X - location.X) * (p.X - location.X) + (p.Y - location.Y) * (p.Y - location.Y) <=
+            ratio / div * (ratio / div))
+          return true;
+        return false;
+      }
+
+      p = i.GetOffsetPoint(p);
+      var offsetPoint = i.GetOffsetPoint(location);
+      if ((p.X - offsetPoint.X) * (p.X - offsetPoint.X) + (p.Y - offsetPoint.Y) * (p.Y - offsetPoint.Y) <=
+          ratio * ratio)
+        return true;
+      return false;
+    }
+  }
+}
